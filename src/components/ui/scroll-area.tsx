d74aa702f8b1fd47ba65203e37a54f3d91ab0673
Tharun Kumar <tharun.mallavarapu@willowtreeapps.com>
2026-04-15 18:40:34 -0700

    ui: ScrollArea scrollbar hover-only + subtle styling to match global

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -13,7 +13,7 @@ function ScrollArea({
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
-      className={cn("relative", className)}
+      className={cn("group/scroll relative", className)}
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
@@ -39,14 +39,14 @@ function ScrollBar({
       data-orientation={orientation}
       orientation={orientation}
       className={cn(
-        "flex touch-none p-px transition-colors select-none data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-l data-vertical:border-l-transparent",
+        "flex touch-none p-px select-none opacity-0 transition-opacity duration-300 group-hover/scroll:opacity-100 data-horizontal:h-1.5 data-horizontal:flex-col data-vertical:h-full data-vertical:w-1.5",
         className
       )}
       {...props}
     >
       <ScrollAreaPrimitive.Thumb
         data-slot="scroll-area-thumb"
-        className="relative flex-1 rounded-full bg-border"
+        className="relative flex-1 rounded-full bg-black/[0.12] hover:bg-black/[0.2] transition-colors"
       />
     </ScrollAreaPrimitive.Scrollbar>
   )
